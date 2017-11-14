f455b929e8ae860d8e57c8be53030d68dfc43c1a
MoAir <hassoun@outlook.com>
2017-11-14 10:46:54 -0500

    add radius_of_curvature

--- sliding_windows.py
+++ sliding_windows.py
@@ -9,13 +9,16 @@ def fit_polynomial(binary_warped):
     # Set height of windows
     window_height = np.int(binary_warped.shape[0]/nwindows)
     window_width = 50
+    margin = 100 # How much to slide left and right for searching
+
 
     def main(binary_warped):
         window_centroids = convolve(binary_warped)
-      
+        
         # If we found any window centers
         if len(window_centroids) > 0:
-
+            
+            radius_of_curvature(binary_warped,window_centroids)
             # Points used to draw all the left and right windows
             l_points = np.zeros_like(binary_warped)
             r_points = np.zeros_like(binary_warped)
@@ -23,112 +26,70 @@ def fit_polynomial(binary_warped):
             # Go through each level and draw the windows 	
             for level in range(0,len(window_centroids)):
                 # Window_mask is a function to draw window areas
+       
                 l_mask = window_mask(window_width,window_height,binary_warped,window_centroids[level][0],level)
                 r_mask = window_mask(window_width,window_height,binary_warped,window_centroids[level][1],level)
                 # Add graphic points from window mask here to total pixels found 
                 l_points[(l_points == 255) | ((l_mask == 1) ) ] = 255
                 r_points[(r_points == 255) | ((r_mask == 1) ) ] = 255
-
-         
+  
+    
             # Draw the results
             template = np.array(r_points+l_points,np.uint8) # add both left and right window pixels together
             zero_channel = np.zeros_like(template) # create a zero color channel
             template = np.array(cv2.merge((zero_channel,template,zero_channel)),np.uint8) # make window pixels green
             warpage= np.dstack((binary_warped, binary_warped, binary_warped))*255 # making the original road pixels 3 color channels
-            output = cv2.addWeighted(warpage, 1, template, 0.5, 0.0) # overlay the original road image with window results
-        
+            output = cv2.addWeighted(warpage, .7, template, 1, 0.0) # overlay the original road image with window results
+           
         # If no window centers found, just display original road image
         else:
             output = np.array(cv2.merge((binary_warped,binary_warped,binary_warped)),np.uint8)
         
         return output
 
-    def histogram(binary_warped):
-        # Take a histogram of the bottom half of the image
-        histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
-        # Create an output image to draw on and  visualize the result
-        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-        # Find the peak of the left and right halves of the histogram
-        # These will be the starting point for the left and right lines
-        midpoint = np.int(histogram.shape[0]/2)
-        leftx_base = np.argmax(histogram[:midpoint])
-        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+    # compute the radius of curvature of the fit
+    def radius_of_curvature(binary_warped,window_centroids):
+        ploty = np.linspace(0, binary_warped.shape[0], num=nwindows) # to cover same y-range as image
         
-        
-        # Identify the x and y positions of all nonzero pixels in the image
-        nonzero = binary_warped.nonzero()
-        nonzeroy = np.array(nonzero[0])
-        nonzerox = np.array(nonzero[1])
-        # Current positions to be updated for each window
-        leftx_current = leftx_base
-        rightx_current = rightx_base
-        # Set the width of the windows +/- margin
-        margin = 100
-        # Set minimum number of pixels found to recenter window
-        minpix = 50
-        # Create empty lists to receive left and right lane pixel indices
-        left_lane_inds = []
-        right_lane_inds = []
-        
-        # Step through the windows one by one
-        for window in range(nwindows):
-            # Identify window boundaries in x and y (and right and left)
-            win_y_low = binary_warped.shape[0] - (window+1)*window_height
-            win_y_high = binary_warped.shape[0] - window*window_height
-            win_xleft_low = leftx_current - margin
-            win_xleft_high = leftx_current + margin
-            win_xright_low = rightx_current - margin
-            win_xright_high = rightx_current + margin
-            # Draw the windows on the visualization image
-            cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
-            (0,255,0), 2) 
-            cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
-            (0,255,0), 2) 
-            # Identify the nonzero pixels in x and y within the window
-            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
-            (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
-            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
-            (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
-            # Append these indices to the lists
-            left_lane_inds.append(good_left_inds)
-            right_lane_inds.append(good_right_inds)
-            # If you found > minpix pixels, recenter next window on their mean position
-            if len(good_left_inds) > minpix:
-                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-            if len(good_right_inds) > minpix:        
-                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+        levels = [level for level in window_centroids]                
+        leftx = np.array([left for left,right in levels])
+        rightx = np.array([right for left,right in levels])
 
-        # Concatenate the arrays of indices
-        left_lane_inds = np.concatenate(left_lane_inds)
-        right_lane_inds = np.concatenate(right_lane_inds)
-        
-        # Extract left and right line pixel positions
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds] 
-        rightx = nonzerox[right_lane_inds]
-        righty = nonzeroy[right_lane_inds] 
-
-        # Fit a second order polynomial to each
-        left_fit = np.polyfit(lefty, leftx, 2)
-        right_fit = np.polyfit(righty, rightx, 2)
-
-        # visualization
-        # Generate x and y values for plotting
-        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+        left_fit = np.polyfit(ploty, leftx, 2)
         left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+        right_fit = np.polyfit(ploty, rightx, 2)
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]    
 
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-        # plt.imshow(out_img)
-        # plt.plot(left_fitx, ploty, color='yellow')
-        # plt.plot(right_fitx, ploty, color='yellow')
+        # mark_size = 3
+        # plt.plot(leftx, ploty, 'o', color='red', markersize=mark_size)
+        # plt.plot(rightx, ploty, 'o', color='blue', markersize=mark_size)
         # plt.xlim(0, 1280)
-        # plt.ylim(720, 0)
+        # plt.ylim(0, 720)
+        # plt.plot(left_fitx, ploty, color='green', linewidth=3)
+        # plt.plot(right_fitx, ploty, color='green', linewidth=3)
+        # plt.gca().invert_yaxis() # to visualize as we do the images
         # plt.show()
-        return out_img
+        # Define y-value where we want radius of curvature
+        # I'll choose the maximum y-value, corresponding to the bottom of the image
+        y_eval = binary_warped.shape[0]
+        # left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+        # right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+        #print(left_curverad, right_curverad)
+        
+        # Define conversions in x and y from pixels space to meters
+        ym_per_pix = 30/binary_warped.shape[0] # meters per pixel in y dimension
+        xm_per_pix = 3.7/binary_warped.shape[1] # meters per pixel in x dimension
+       
+        # Fit new polynomials to x,y in world space
+        left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+        right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+        # Calculate the new radii of curvature
+        left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+        right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+        # Now our radius of curvature is in meters
+        print(left_curverad, 'm', right_curverad, 'm')
+        #exit(0)
 
-    
     def window_mask(width, height, img_ref, center,level):
         output = np.zeros_like(img_ref)
         output[int(img_ref.shape[0]-(level+1)*height):int(img_ref.shape[0]-level*height),max(0,int(center-width/2)):min(int(center+width/2),img_ref.shape[1])] = 1
@@ -137,25 +98,37 @@ def fit_polynomial(binary_warped):
     def convolve(binary_warped):
         image = binary_warped
         window_centroids = [] # Store the (left,right) window centroid positions per level
-        margin = 100 # How much to slide left and right for searching
         window = np.ones(window_width) # Create our window template that we will use for convolutions
+
         # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
         # and then np.convolve the vertical image slice with the window template 
-        
-        # Sum quarter bottom of image to get slice, could use a different ratio
-        l_sum = np.sum(image[int(3*image.shape[0]/4):,:int(image.shape[1]/2)], axis=0)
-        l_center = np.argmax(np.convolve(window,l_sum))-window_width/2
-        r_sum = np.sum(image[int(3*image.shape[0]/4):,int(image.shape[1]/2):], axis=0)
-        r_center = np.argmax(np.convolve(window,r_sum))-window_width/2+int(image.shape[1]/2)
 
-         # Add what we found for the first layer
+        # get the quarter bottom  image, slicing horizontally  
+        bottom_image = image[int(image.shape[0]*(3/4)):]
+        
+        # divide images into left and right
+        l_img = bottom_image[:,:int(image.shape[1]/2)]
+        r_img = bottom_image[:,int(image.shape[1]/2):]
+        # Sum 
+        l_sum = np.sum(l_img, axis=0)
+        r_sum = np.sum(r_img, axis=0)
+        # apply a convolution to maximize the number of "hot" pixels in each window
+        l_convolve = np.convolve(window,l_sum,'same')
+        r_convolve = np.convolve(window,r_sum,'same')
+
+        # Returns the max index of the maximum value
+        l_center = np.argmax(l_convolve)
+        r_center = np.argmax(r_convolve)+ int(image.shape[1]/2)
+
+        # Add what we found for the first layer
         window_centroids.append((l_center,r_center))
 
          # Go through each layer looking for max pixel locations
         for level in range(1,(int)(image.shape[0]/window_height)):
             # convolve the window into the vertical slice of the image
-            image_layer = np.sum(image[int(image.shape[0]-(level+1)*window_height):int(image.shape[0]-level*window_height),:], axis=0)
-            conv_signal = np.convolve(window, image_layer)
+            image_layer = image[int(image.shape[0]-(level+1)*window_height):int(image.shape[0]-level*window_height),:]
+            image_layer_sum = np.sum(image_layer, axis=0)
+            conv_signal = np.convolve(window, image_layer_sum)
             # Find the best left centroid by using past left center as a reference
             # Use window_width/2 as offset because convolution signal reference is at right side of window, not center of window
             offset = window_width/2
